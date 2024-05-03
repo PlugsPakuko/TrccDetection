6448c5b2b67746aaaa6a92450f507103854516c0
Pakkawat K <plugs2004@gmail.com>
2024-05-03 18:27:52 +0700

    Update Requirement

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # TODO List
 - [x] Change Detection to function, return coordinate of the nearest
 - [x] Get Nearest Ball Function
-- [] Time Encoder
+- [] Time Encoder(?)
 - [] Check Empty Silo/ Strategy
 - [] Count ball within Silo 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import cv2
 import keyboard
 
+Silo = 0
 BallBlue = 1
 BallPurple = 2
 BallRed = 3
@@ -10,7 +11,7 @@ cap = cv2.VideoCapture(0)
 model = YOLO("best.pt")
 classNames = ['Silo', 'ball_blue', 'ball_purple', 'ball_red']
 
-def get_ball(target_ball):
+def get_ball(TargetBall):
     nearest_target = (None, None)
     min_d = float('inf')
 
@@ -20,7 +21,7 @@ def get_ball(target_ball):
     for r in results:
         box = r.boxes
 
-        if box and classNames[int(box.cls[0])] == classNames[target_ball]:
+        if box and classNames[int(box.cls[0])] == classNames[TargetBall]:
             x1, y1, x2, y2 = box.xyxy[0]
             centroid_x = ( float(x1) + float(x2) ) / 2
             centroid_y = ( float(y1) + float(y2) ) / 2
@@ -39,7 +40,7 @@ def get_ball(target_ball):
 
 while True:
     print("Waiting...")
-    if keyboard.read_key() == 'p':
+    if keyboard.read_key() == 'p': 
         coordinate = get_ball(BallBlue)
     else:
         break
